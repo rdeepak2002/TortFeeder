243d2c9d8352da84b0efa93f633c91aed244b179
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-09 23:35:39 -0700

    testing

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
 # Import libraries
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 from flask import Flask
 
 # Set GPIO numbering mode
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setmode(GPIO.BOARD)
 
-# Set pin 11 as an output, and define as servo1 as PWM pin
-GPIO.setup(11,GPIO.OUT)
-servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
+# # Set pin 11 as an output, and define as servo1 as PWM pin
+# GPIO.setup(11,GPIO.OUT)
+# servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
 
-# Start PWM running, with value of 0 (pulse off)
-servo1.start(0)
+# # Start PWM running, with value of 0 (pulse off)
+# servo1.start(0)
 
-# Define variables
-startAngle = 180
-endAngle = 140
+# # Define variables
+# startAngle = 180
+# endAngle = 140
 
-# Method to set the angle
-def setAngle(angle):
-	servo1.ChangeDutyCycle(2+(angle/18))
-	time.sleep(0.2)
-	servo1.ChangeDutyCycle(0)
+# # Method to set the angle
+# def setAngle(angle):
+# 	servo1.ChangeDutyCycle(2+(angle/18))
+# 	time.sleep(0.2)
+# 	servo1.ChangeDutyCycle(0)
 
-# Feed method to quickly change angle and dispense food
-def feed():
-	setAngle(startAngle)
-	setAngle(endAngle)
-	setAngle(startAngle)
+# # Feed method to quickly change angle and dispense food
+# def feed():
+# 	setAngle(startAngle)
+# 	setAngle(endAngle)
+# 	setAngle(startAngle)
 
-# Start the motor at the necessary start angle
-setAngle(startAngle)
+# # Start the motor at the necessary start angle
+# setAngle(startAngle)
 
-# Call feed to dispense food
-feed()
+# # Call feed to dispense food
+# feed()
 
 app = Flask(__name__)
 
