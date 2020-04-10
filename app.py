2c1a197708118fe7eb371fcaec8df52cfbf0be50
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-10 00:07:25 -0700

    removed ssl cert

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def add_header(r):
 # Run the server
 if __name__ == "__main__":
 	useDebug = True
-	app.run(host='0.0.0.0', port=5000, ssl_context='adhoc', debug=useDebug)
+	app.run(host='0.0.0.0', port=5000, debug=useDebug)
