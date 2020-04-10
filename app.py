5a420d7eb48e353cdcfeb922d3abbe69b20d3785
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-09 22:45:19 -0700

    added method

--- app.py
+++ app.py
@@ -12,20 +12,14 @@ servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
 # Start PWM running, with value of 0 (pulse off)
 servo1.start(0)
 
-# Loop to allow user to set servo angle. Try/finally allows exit
-# with execution of servo.stop and GPIO cleanup :)
+setAngle(180)
 
-try:
-    while True:
-        #Ask user for angle and turn servo to it
-        angle = float(input('Enter angle between 0 & 180: '))
-        servo1.ChangeDutyCycle(2+(angle/18))
-        time.sleep(0.5)
-        servo1.ChangeDutyCycle(0)
+setAngle(90)
 
-finally:
-    #Clean things up at the end
-    servo1.stop()
-    GPIO.cleanup()
-    print("Goodbye!")
+def setAngle(angle):
+    servo1.ChangeDutyCycle(2+(angle/18))
+    time.sleep(0.5)
+    servo1.ChangeDutyCycle(0)
 
+servo1.stop()
+GPIO.cleanup()
