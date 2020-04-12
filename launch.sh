aa9746d540220e63f0604c52b28164877d4a0fdc
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-12 01:51:29 -0700

    fixed port

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 cd /home/pi/TortFeeder
-sudo gunicorn --timeout 90 --certfile certificate.crt --keyfile private.key --bind 0.0.0.0:5000 app:app
+sudo gunicorn --timeout 90 --certfile certificate.crt --keyfile private.key --bind 0.0.0.0:443 app:app
