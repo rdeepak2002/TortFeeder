a14db7e974467201fc020d94cf651af156ee876b
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-12 01:48:35 -0700

    fixed launch

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 cd /home/pi/TortFeeder
-sudo gunicorn --timeout 90 --threads 5 --workers 4 --bind 0.0.0.0:5000 app:app
+sudo gunicorn --timeout 90 --certfile certificate.crt --keyfile private.key --bind 0.0.0.0:5000 app:app
